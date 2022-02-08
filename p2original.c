5ccdcbfaad2deb668589c65a53d7e863cdd8e983
Kiransuraj <96763670+Kiransuraj@users.noreply.github.com>
2022-02-08 07:28:33 +0000

    Completed

--- p2original.c
+++ p2original.c
@@ -1,1 +1,41 @@
 #include<stdio.h>
+int input_side()
+{
+  int n;
+  printf("Enter the side of the triangle:");
+  scanf("%d",&n);
+  return n;
+}
+int check_scalene(int a, int b, int c)
+{
+  int isscalene;
+  if((a!=b)&&(b!=c)&&(c!=a))
+  {
+   return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+void output(int a, int b, int c,int isscalene)
+{
+  if(isscalene==1)
+  {
+   printf("Is scalene\n");
+  }
+  else
+ {
+   printf("It is not scalene\n");
+ }
+} 
+int main()
+{
+  int a,b,c,isscalene;
+  a=input_side();
+  b=input_side();
+  c=input_side();
+  isscalene=check_scalene(a,b,c);
+  output(a,b,c,isscalene);
+  return 0;
+}
---
+++ p3original.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int input_number()
+{
+  int n;
+  printf("Enter the number:");
+  scanf("%d",&n);
+  return n;
+}
+int is_composite(int n)
+{
+  int c;
+  for(int i=0;i<=n;i++)
+  {
+    c=n/
+  }
+  return c;
+}
